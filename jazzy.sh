a8e63dfd61832220725b1db03aa8ad7f95990268
Alex Rupérez <me@alexruperez.com>
2020-04-16 14:40:18 +0000

    Documentation update.

--- jazzy.sh
+++
@@ -1,7 +0,0 @@
-mkdir -p docs
-sourcekitten doc --spm-module Storage > docs/Storage.json
-sourcekitten doc --spm-module Keychain > docs/Keychain.json
-sourcekitten doc --spm-module UserDefault > docs/UserDefault.json
-sourcekitten doc --spm-module Singleton > docs/Singleton.json
-sourcekitten doc --spm-module Inject > docs/Inject.json
-jazzy --config .jazzy.yml
